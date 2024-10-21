68723f512af38319778100cac6f8ac19644f9ad8
Jay Zhang <wangyoucao577@gmail.com>
2024-10-21 05:50:25 +0000

    fix: log

--- src/log.h
+++ src/log.h
@@ -7,11 +7,11 @@
  */
 #pragma once
 
-#define AZURE_TTS_LOG_TAG "AZURE_TTS_EXTENSION"
+#define AZURE_TTS_LOG_TAG "[azure_tts_extension] "
 
 #include "ten_utils/log/log.h"
 
-#define AZURE_TTS_LOGI(...) TEN_LOGI(AZURE_TTS_LOG_TAG, __VA_ARGS__)
-#define AZURE_TTS_LOGE(...) TEN_LOGE(AZURE_TTS_LOG_TAG, __VA_ARGS__)
-#define AZURE_TTS_LOGW(...) TEN_LOGW(AZURE_TTS_LOG_TAG, __VA_ARGS__)
-#define AZURE_TTS_LOGD(...) TEN_LOGD(AZURE_TTS_LOG_TAG, __VA_ARGS__)
+#define AZURE_TTS_LOGI(...) TEN_LOGI(AZURE_TTS_LOG_TAG __VA_ARGS__)
+#define AZURE_TTS_LOGE(...) TEN_LOGE(AZURE_TTS_LOG_TAG __VA_ARGS__)
+#define AZURE_TTS_LOGW(...) TEN_LOGW(AZURE_TTS_LOG_TAG __VA_ARGS__)
+#define AZURE_TTS_LOGD(...) TEN_LOGD(AZURE_TTS_LOG_TAG __VA_ARGS__)
